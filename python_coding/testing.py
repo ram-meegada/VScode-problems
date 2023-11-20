7b4227eebf4017da5e4e0a08f648a37b5e0bde7b
ramu-meegada <ramumeegada1811@gmail.com>
2023-11-20 10:26:38 +0530

    changes

--- python_coding/testing.py
+++ python_coding/testing.py
@@ -1,8 +1,30 @@
-import json
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent
+# , with the colors in the order red, white, and blue.
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+# You must solve this problem without using the library's sort function.
+# Example 1:
+# Input: nums = [2,0,2,1,1,0]
+# Output: [0,0,1,1,2,2]
+# Example 2:
+# Input: nums = [2,0,1]
+# Output: [0,1,2]
 
-n = {'question1': 
- 			[{'question3': [{'question4': []}, {'question7': []}]}], 'question2': [{'question5': []}, {'question6': []}]}
-n = json.dumps(n)
-n = n.replace("'", '"')
-n = json.loads(n)
-print(json.dumps(n, indent=2))
+def solution(nums):
+    l = 0 
+    r = len(nums)-1
+    while l < r:
+        if nums[l] > nums[r]:
+            temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
+            r -= 1
+        elif nums[l] < nums[r]:
+            l += 1
+        else:
+            l += 1
+            r -= 1
+    return nums
+
+nums = [1,0,2]
+result = solution(nums)
+print(result)
